ac89f3fbdf7d5da31116ea5e0aa9a0b772e3c6f8
nikomark <markovicnikolaj@gmail.com>
2015-11-11 10:20:08 +0100

    adding fibdp

---
+++ dynamicProg/fibonaccidp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int fibdp(int);
+
+int main(){
+	
+	int n;
+	cout<<"n: ";
+	cin>>n;
+	cout<<fibdp(n)<<endl;
+}
+
+int fibdp(int n){
+
+	if(n==0)
+		return 0;
+	else
+	{
+		int prev=0, curr=1;
+		for(int i=1;i<n; i++)
+		{
+			int newFib=prev+curr;
+			prev=curr;
+			curr=newFib;
+		}
+		return curr;
+	}
+}
